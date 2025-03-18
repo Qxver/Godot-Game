29b11e6a0f06fa1594c03081aacf573ca619d346
SmalecWalec <62522955+SmalecWalec@users.noreply.github.com>
2025-03-18 22:54:30 +0100

    Fixed pause menu + camera moves with character

--- Menu/pause_menu.gd
+++ Menu/pause_menu.gd
@@ -1,17 +1,24 @@
 extends Control
 
+func _ready():
+	hide()
+	$AnimationPlayer.play("RESET")
+
 func resume():
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
-
+	await $AnimationPlayer.animation_finished
+	hide()
+	
 func pause():
+	show()
 	get_tree().paused = true
 	$AnimationPlayer.play("blur")
 
 func testEsc():
 	if Input.is_action_just_pressed("esc") and !get_tree().paused:
 		pause()
-	elif Input.is_action_just_pressed("esc") and get_tree().pause:
+	elif Input.is_action_just_pressed("esc") and get_tree().paused:
 		resume()
 
 func _on_resume_pressed():
@@ -27,5 +34,5 @@ func _on_main_menu_pressed():
 func _on_quit_pressed():
 	get_tree().quit()
 
-func _process(delta):
+func _process(_delta):
 	testEsc()
