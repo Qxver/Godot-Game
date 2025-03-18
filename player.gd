7689bab356d2dbf9880338bd8db078b5e05d6506
Wojciech Paradziński <paradzinskiwojciech@gmail.com>
2025-03-18 21:30:22 +0100

    Character2D, Terrain
    Added idle and walk animation
    Added terrain

--- player.gd
+++ player.gd
@@ -2,6 +2,15 @@ extends CharacterBody2D
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down") # character movement
-	velocity = direction * 500 # character speed
+	velocity = direction * 100 # character speed
 	move_and_slide()
 	
+	if Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right") or Input.is_action_pressed("move_up") or Input.is_action_pressed("move_down"):
+		$AnimatedSprite2D.play("walk")
+		if Input.is_action_pressed("move_left"):
+			$AnimatedSprite2D.flip_h = true
+		if Input.is_action_pressed("move_right"):
+			$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.play("idle")
+		
