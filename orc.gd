e02ace5b74b7519d27a2b4dc8426601193a4f864
Wojciech Paradziński <paradzinskiwojciech@gmail.com>
2025-03-21 12:52:06 +0100

    Mob movement animation

--- orc.gd
+++ orc.gd
@@ -4,9 +4,17 @@ var player
 
 func _ready() -> void:
 	player = get_node("/root/Game/Player")
+	$AnimatedSprite2D.play("walk")
+		
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 40  # mob speed
-	move_and_slide()
 	
+	if player:
+		var direction2 = (player.global_position.x - global_position.x)
+		
+		if abs(direction2) > 5:
+			velocity.x = sign(direction2) * 40
+			$AnimatedSprite2D.flip_h = direction2 < 0
+			move_and_slide()
