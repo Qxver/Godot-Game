5011e351369161f909c87dfd59198ecb23eddb0f
kajryg <151868201+kajryg@users.noreply.github.com>
2025-03-28 18:48:53 +0100

    Added functions related to dealing damage

--- Attacks/bow.gd
+++ Attacks/bow.gd
@@ -1,28 +1,37 @@
 extends Area2D
 
+#Weapon stats
 var level = 1
 var pierce = 1 
 var speed = 100
-var damage = 5
+var damage = 60
 var attacksize = 1.0
 
+#Aiming stats
 var target = Vector2.ZERO
 var angle = Vector2.ZERO
 
 @onready var player = get_tree().get_first_node_in_group("player")
 
+#setting rotation to enemy
 func _ready() -> void:
 	angle = global_position.direction_to(target)
-	rotation = angle.angle() + deg_to_rad(135)	
-		
-			
+	rotation = angle.angle() + deg_to_rad(180)	
+
+#moving projectiles
 func _physics_process(delta) -> void:
 		global_position += angle * speed * delta
-	
+
+#Deleting projectiles after hitting enough enemies
 func enemy_hit() -> void:
 	pierce -= 1
-	if pierce < 0:
+	if pierce <= 0:
 		queue_free()
 
-func _on_timer_timeout() -> void: #duration
+func DealDamage():
+	enemy_hit()
+	return damage
+
+#Deleting projectiles after some time
+func _on_duration_timeout() -> void: 
 	queue_free()
