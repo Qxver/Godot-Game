de1dd9a33d31ef3a1e92cc199359819863cc269f
Wojciech Paradziński <paradzinskiwojciech@gmail.com>
2025-03-29 13:33:01 +0100

    Enemy spawn time reduction based on time

--- game.gd
+++ game.gd
@@ -12,7 +12,7 @@ func _on_timer_timeout() -> void:
 	spawn_enemy()
 
 # screen timer
-var elapsed_time := 0
+var elapsed_time: int= 0  # time (seconds)
 func _on_timer_timeout2() -> void:
 	elapsed_time += 1
 	var minutes = elapsed_time / 60
@@ -21,3 +21,12 @@ func _on_timer_timeout2() -> void:
 
 func _ready() -> void:
 	%Label.set_text("0:00")
+	
+# change mob spawn time -0.1s every 10s elapsed time
+var time_passed = 0
+func _physics_process(delta: float) -> void:
+	time_passed += delta
+	if %TimerMobSpawn.wait_time >= 0.1:
+		if time_passed >= 10:
+			time_passed = 0
+			%TimerMobSpawn.wait_time -= 0.1
