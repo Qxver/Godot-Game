0b301727e93ad53a5df8a2a551d7a437a77e8817
kajryg <151868201+kajryg@users.noreply.github.com>
2025-04-26 17:05:13 +0200

    Added new weapon Bomb

---
+++ Attacks/bomb.gd
@@ -0,0 +1,48 @@
+extends Area2D
+#Weapon stats
+var level = 1
+var speed = 100
+var damage_multiplier = 0.25
+var attacksize = 1.0
+var duration =  1.0
+
+var target = Vector2.ZERO
+@onready var player = get_tree().get_first_node_in_group("player")
+var can_move: bool = true
+
+#setting rotation to enemy
+func _ready() -> void:
+	add_to_group("Attacks")
+	add_to_group("Stunners")
+	rotation = target.angle() + deg_to_rad(180)	
+	$Explosion.disabled = true
+	$Sprite2D2.visible = false
+
+#moving projectiles
+func _physics_process(delta) -> void:
+	if can_move:
+		global_position += target * speed * delta
+
+#Deleting projectiles after hitting enough enemies
+func enemy_hit() -> void:
+	explosion()
+
+func stun():
+	return duration
+
+func DealDamage():
+	enemy_hit()
+	return PlayerStats.damage*damage_multiplier
+	
+func explosion() -> void:
+	can_move=false
+	damage_multiplier = 0.75
+	$Sprite2D2.visible=true
+	$Explosion.disabled = true
+	$BombExplsionTime.start()
+	
+func _on_timer_timeout() -> void:
+	explosion()
+
+func _on_bomb_explsion_time_timeout() -> void:
+	queue_free()
